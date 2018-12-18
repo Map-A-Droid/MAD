022d932e385845d14e43d90e53a7b1461e6b2df7
Sascha Ohms <sasch9r@gmail.com>
2018-12-19 00:34:37 +0100

    fix monocle fort ids in madmin map

--- db/monocleWrapper.py
+++ db/monocleWrapper.py
@@ -465,7 +465,7 @@ class MonocleWrapper(DbWrapperBase):
             "SELECT forts.external_id, forts.lat, forts.lon, forts.name, forts.url, "
             "IFNULL(forts.park, 'unknown'), forts.sponsor, fort_sightings.team "
             "FROM forts "
-            "INNER JOIN fort_sightings ON forts.id = fort_sightings.id"
+            "INNER JOIN fort_sightings ON forts.id = fort_sightings.fort_id"
         )
 
         res = self.execute(query)
