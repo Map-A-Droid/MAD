354295a73635cb6997f5cf4a92c95067ef481c2a
Sascha Ohms <sasch9r@gmail.com>
2019-01-17 09:45:22 +0100

    change raid pokemon key to reflect latest changes

--- db/rmWrapper.py
+++ db/rmWrapper.py
@@ -830,10 +830,10 @@ class RmWrapper(DbWrapperBase):
                 if gym['type'] == 0 and gym['gym_details']['has_raid']:
                     gym_has_raid = gym['gym_details']['raid_info']['has_pokemon']
                     if gym_has_raid:
-                        pokemon_id = gym['gym_details']['raid_info']['pokemon']['id']
-                        cp = gym['gym_details']['raid_info']['pokemon']['cp']
-                        move_1 = gym['gym_details']['raid_info']['pokemon']['move_1']
-                        move_2 = gym['gym_details']['raid_info']['pokemon']['move_2']
+                        pokemon_id = gym['gym_details']['raid_info']['raid_pokemon']['id']
+                        cp = gym['gym_details']['raid_info']['raid_pokemon']['cp']
+                        move_1 = gym['gym_details']['raid_info']['raid_pokemon']['move_1']
+                        move_2 = gym['gym_details']['raid_info']['raid_pokemon']['move_2']
                     else:
                         pokemon_id = None
                         cp = 0
