9933391e273f14fbc92c19a10efe701d827a70d4
cecpk <cec@retrospot.de>
2019-01-30 23:38:05 +0100

    Add Timer for device

--- websocket/WebsocketServerBase.py
+++ websocket/WebsocketServerBase.py
@@ -4,15 +4,17 @@ import logging
 import math
 import queue
 import sys
+import time
 from abc import ABC
 from threading import Lock, Event, Thread
 
 import websockets
 
 from utils.authHelper import check_auth
-from utils.madGlobals import WebsocketWorkerRemovedException, MadGlobals
+from utils.madGlobals import WebsocketWorkerRemovedException
 from worker.WorkerMITM import WorkerMITM
 from worker.WorkerQuests import WorkerQuests
+from utils.timer import Timer
 
 log = logging.getLogger(__name__)
 OutgoingMessage = collections.namedtuple('OutgoingMessage', ['id', 'message'])
@@ -97,6 +99,8 @@ class WebsocketServerBase(ABC):
 
         lastKnownState = {}
         client_mapping = self.device_mappings[id]
+        timer = Timer(client_mapping["switch"], id, client_mapping["switch_interval"])
+        time.sleep(1)
         daytime_routemanager = self.routemanagers[client_mapping["daytime_area"]].get("routemanager")
         if client_mapping.get("nighttime_area", None) is not None:
             nightime_routemanager = self.routemanagers[client_mapping["nighttime_area"]].get("routemanager", None)
@@ -105,38 +109,38 @@ class WebsocketServerBase(ABC):
         devicesettings = client_mapping["settings"]
 
         started = False
-        if MadGlobals.sleep is True:
+        if timer.get_switch() is True:
             # start the appropriate nighttime manager if set
             if nightime_routemanager is None:
                 pass
             elif nightime_routemanager.mode in ["raids_mitm", "mon_mitm", "iv_mitm"]:
                 worker = WorkerMITM(self.args, id, lastKnownState, self, daytime_routemanager, nightime_routemanager,
-                                    self._mitm_mapper, devicesettings, db_wrapper=self.db_wrapper)
+                                    self._mitm_mapper, devicesettings, db_wrapper=self.db_wrapper, timer=timer)
                 started = True
             elif nightime_routemanager.mode in ["raids_ocr"]:
                 from worker.WorkerOcr import WorkerOcr
                 worker = WorkerOcr(self.args, id, lastKnownState, self, daytime_routemanager, nightime_routemanager,
-                                   devicesettings, db_wrapper=self.db_wrapper)
+                                   devicesettings, db_wrapper=self.db_wrapper, timer=timer)
                 started = True
             elif nightime_routemanager.mode in ["pokestops"]:
                 worker = WorkerQuests(self.args, id, lastKnownState, self, daytime_routemanager, nightime_routemanager,
-                                      self._mitm_mapper, devicesettings, db_wrapper=self.db_wrapper)
+                                      self._mitm_mapper, devicesettings, db_wrapper=self.db_wrapper, timer=timer)
                 started = True
             else:
                 log.fatal("Mode not implemented")
                 sys.exit(1)
-        if not MadGlobals.sleep or not started:
+        if not timer.get_switch() or not started:
             # we either gotta run daytime mode OR nighttime routemanager not set
             if daytime_routemanager.mode in ["raids_mitm", "mon_mitm", "iv_mitm"]:
                 worker = WorkerMITM(self.args, id, lastKnownState, self, daytime_routemanager, nightime_routemanager,
-                                    self._mitm_mapper, devicesettings, db_wrapper=self.db_wrapper)
+                                    self._mitm_mapper, devicesettings, db_wrapper=self.db_wrapper, timer=timer)
             elif daytime_routemanager.mode in ["raids_ocr"]:
                 from worker.WorkerOcr import WorkerOcr
                 worker = WorkerOcr(self.args, id, lastKnownState, self, daytime_routemanager, nightime_routemanager,
-                                   devicesettings, db_wrapper=self.db_wrapper)
+                                   devicesettings, db_wrapper=self.db_wrapper, timer=timer)
             elif daytime_routemanager.mode in ["pokestops"]:
                 worker = WorkerQuests(self.args, id, lastKnownState, self, daytime_routemanager, nightime_routemanager,
-                                      self._mitm_mapper, devicesettings, db_wrapper=self.db_wrapper)
+                                      self._mitm_mapper, devicesettings, db_wrapper=self.db_wrapper, timer=timer)
             else:
                 log.fatal("Mode not implemented")
                 sys.exit(1)
--- worker/WorkerMITM.py
+++ worker/WorkerMITM.py
@@ -4,7 +4,7 @@ import time
 
 from route.RouteManagerIV import RouteManagerIV
 from utils.geo import get_distance_of_two_points_in_meters
-from utils.madGlobals import MadGlobals, InternalStopWorkerException
+from utils.madGlobals import InternalStopWorkerException
 from worker.WorkerBase import WorkerBase
 
 log = logging.getLogger(__name__)
@@ -120,9 +120,9 @@ class WorkerMITM(WorkerBase):
         return reached_raidtab
 
     def __init__(self, args, id, last_known_state, websocket_handler, route_manager_daytime, route_manager_nighttime,
-                 mitm_mapper, devicesettings, db_wrapper):
+                 mitm_mapper, devicesettings, db_wrapper, timer):
         WorkerBase.__init__(self, args, id, last_known_state, websocket_handler, route_manager_daytime,
-                            route_manager_nighttime, devicesettings, db_wrapper=db_wrapper, NoOcr=True)
+                            route_manager_nighttime, devicesettings, db_wrapper=db_wrapper, NoOcr=True, timer=timer)
         self._mitm_mapper = mitm_mapper
         # TODO: own InjectionSettings class
         self._injection_settings = {}
