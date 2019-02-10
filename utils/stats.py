80bc38b48eab10039e33472f8a23e5cc20d53a06
cecpk <cec@retrospot.de>
2019-02-10 19:43:37 +0100

    FIX Worker Stats

--- utils/stats.py
+++ utils/stats.py
@@ -28,40 +28,29 @@ class PlayerStats(object):
             for data_inventory in stats:
                 player_level = data_inventory['inventory_item_data']['player_stats']['level']
                 if int(player_level) > 0:
-                    
-                    if os.path.exists('status.json'):
-                        with open('status.json') as f:
-                            data = json.load(f)
-                    else:
-                        data = {}
-                    
                     log.debug('{{gen_player_stats}} saving new playerstats')
                     self.set_level(int(player_level))
                             
-                    data[self._id]['player'] = {  
+                    data = {}  
+                    data[self._id] = []
+                    data[self._id].append({  
                         'level': str(data_inventory['inventory_item_data']['player_stats']['level']), 
                         'experience': str(data_inventory['inventory_item_data']['player_stats']['experience']),
                         'km_walked': str(data_inventory['inventory_item_data']['player_stats']['km_walked']),
                         'pokemons_encountered': str(data_inventory['inventory_item_data']['player_stats']['pokemons_encountered']),
                         'poke_stop_visits': str(data_inventory['inventory_item_data']['player_stats']['poke_stop_visits'])
-                    }
-                    with open('status.json', 'w') as outfile:  
+                    })
+                    with open(self._id + '.stats', 'w') as outfile:  
                         json.dump(data, outfile, indent=4, sort_keys=True)
                         
     def _open_player_stats(self):
-        statsfile = Path('status.json')
+        statsfile = Path(str(self._id) + '.stats')
         if not statsfile.is_file():
             log.error('[%s] - no Statsfile found' % (str(self._id)))
             self.set_level(0)
             return False
             
-        with open('status.json') as f:
+        with open(str(self._id) + '.stats') as f:
             data = json.load(f)
             
-        if self._id in data:
-            if 'player' in data[self._id]:
-                self.set_level(data[self._id]['player']['level'])
-            else:
-                self.set_level(0)
-        else:
-            self.set_level(0)
+        self.set_level(data[self._id][0]['level'])
