ce38c94378447589da2d4dffdd6b4a5c88872adb
Furtif <msntchat@hotmail.fr>
2019-02-16 21:04:18 +0100

    rename

--- mitm_receiver/MitmMapper.py
+++ mitm_receiver/MitmMapper.py
@@ -1,7 +1,7 @@
 import logging
 from threading import Lock
 from utils.stats import PlayerStats
-from utils.stats import PlayerName
+from utils.stats import PlayerData
 
 log = logging.getLogger(__name__)
 
@@ -10,7 +10,7 @@ class MitmMapper(object):
     def __init__(self, device_mappings):
         self.__mapping = {}
         self._playerstats = {}
-        self._playername = {}
+        self._playerdata = {}
         self.__mapping_mutex = Lock()
         self._device_mappings = device_mappings
         if device_mappings is not None:
@@ -18,8 +18,8 @@ class MitmMapper(object):
                 self.__mapping[origin] = {}
                 self._playerstats[origin] = PlayerStats(origin)
                 self._playerstats[origin]._open_player_stats()
-                self._playername[origin] = PlayerName(origin)
-                self._playername[origin]._open_player_name()
+                self._playerdata[origin] = PlayerData(origin)
+                self._playerdata[origin]._open_player_data()
 
     def get_mon_ids_iv(self, origin):
         if self._device_mappings is None or origin not in self._device_mappings.keys():
--- utils/stats.py
+++ utils/stats.py
@@ -5,7 +5,7 @@ import json
 
 log = logging.getLogger(__name__)
 
-class PlayerName(object):
+class PlayerData(object):
     def __init__(self, name):
         self._name = name
 
@@ -19,36 +19,36 @@ class PlayerName(object):
 
     def _gen_player_name(self, data):
         if 'username' not in data:
-            log.debug('{{gen_player_name}} cannot generate new name')
+            log.debug('{{gen_player_name}} cannot generate new playerdata')
             return True
-        playerdata = data['player_data'].get('player_data', None)
+        playerdata = data['player_data'].get('', None)
 
         if len(playerdata) > 0:
             for data in playerdata:
-                player_name = data['player_data']['username']
+                player_name = data['username']
 
                 if len(data) > 0:
-                    log.debug('{{gen_player_name}} saving new playername')
+                    log.debug('{{gen_player_name}} saving new playerdata')
                     self.set_name(str(player_name))
 
                     data = {}
                     data[self._name] = []
                     data[self._name].append({
-                        'name': str(data['player_data']['username'])
+                        'name': str(data['username'])
                     })
 
-                    with open(self._name + '.playername', 'w') as outfile:
+                    with open(self._name + '.playerdata', 'w') as outfile:
                         json.dump(data, outfile, indent=4, sort_keys=True)
 
 
-    def _open_player_name(self):
-        name = Path(str(self._name) + '.playername')
+    def _open_player_data(self):
+        name = Path(str(self._name) + '.playerdata')
         if not name.is_file():
-            log.error('[%s] - no PlayerName found' % (str(self._name)))
+            log.error('[%s] - no PlayerData found' % (str(self._name)))
             self.set_name(0)
             return False
 
-        with open(str(self._name) + '.playername') as f:
+        with open(str(self._name) + '.playerdata') as f:
             data = json.load(f)
 
         self.set_name(data[self._name][0]['username'])
