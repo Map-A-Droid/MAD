d0144ce56706943919a17422b4760df7ae422d2d
root <root@v22019037984384610.hotsrv.de>
2019-03-12 13:31:48 +0100

    Adding some logging and Exception handling to submit_quest_proto

--- db/dbWrapperBase.py
+++ db/dbWrapperBase.py
@@ -848,43 +848,45 @@ class DbWrapperBase(ABC):
 
     def submit_quest_proto(self, map_proto):
         log.debug("{DbWrapperBase::submit_quest_proto} called")
-        fort_id = map_proto.get("fort_id", None)
-        if fort_id is None:
-            return False
-        if 'challenge_quest' not in map_proto:
-            return False
-        quest_type = map_proto['challenge_quest']['quest'].get("quest_type", None)
-        if map_proto['challenge_quest']['quest'].get("quest_rewards", None):
-            rewardtype = map_proto['challenge_quest']['quest']['quest_rewards'][0].get("type", None)
-            reward = map_proto['challenge_quest']['quest'].get("quest_rewards", None)
-            item = map_proto['challenge_quest']['quest']['quest_rewards'][0]['item'].get("item", None)
-            itemamount = map_proto['challenge_quest']['quest']['quest_rewards'][0]['item'].get("amount", None)
-            stardust = map_proto['challenge_quest']['quest']['quest_rewards'][0].get("stardust", None)
-            pokemon_id = map_proto['challenge_quest']['quest']['quest_rewards'][0]['pokemon_encounter'].get(
-                "pokemon_id", None)
-            target = map_proto['challenge_quest']['quest']['goal'].get("target", None)
-            condition = map_proto['challenge_quest']['quest']['goal'].get("condition", None)
-            
-            task = questtask(int(quest_type), str(condition), int(target))
+        try:
+            fort_id = map_proto.get("fort_id", None)
+            if fort_id is None:
+                log.warning("Cannot submit quest proto : No fort_id set")
+                return False
+            if 'challenge_quest' not in map_proto:
+                log.warning("Cannot submit quest proto : challenge_quest not in map_proto")
+                return False
+            quest_type = map_proto['challenge_quest']['quest'].get("quest_type", None)
+            if map_proto['challenge_quest']['quest'].get("quest_rewards", None):
+                rewardtype = map_proto['challenge_quest']['quest']['quest_rewards'][0].get("type", None)
+                reward = map_proto['challenge_quest']['quest'].get("quest_rewards", None)
+                item = map_proto['challenge_quest']['quest']['quest_rewards'][0]['item'].get("item", None)
+                itemamount = map_proto['challenge_quest']['quest']['quest_rewards'][0]['item'].get("amount", None)
+                stardust = map_proto['challenge_quest']['quest']['quest_rewards'][0].get("stardust", None)
+                pokemon_id = map_proto['challenge_quest']['quest']['quest_rewards'][0]['pokemon_encounter'].get("pokemon_id", None)
+                target = map_proto['challenge_quest']['quest']['goal'].get("target", None)
+                condition = map_proto['challenge_quest']['quest']['goal'].get("condition", None)
 
-            query_quests = (
-                "INSERT into trs_quest (GUID, quest_type, quest_timestamp, quest_stardust, quest_pokemon_id, "
-                "quest_reward_type, quest_item_id, quest_item_amount, quest_target, quest_condition, quest_reward, "
-                "quest_task) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                "ON DUPLICATE KEY UPDATE quest_type=VALUES(quest_type), quest_timestamp=VALUES(quest_timestamp), "
-                "quest_stardust=VALUES(quest_stardust), quest_pokemon_id=VALUES(quest_pokemon_id), "
-                "quest_reward_type=VALUES(quest_reward_type), quest_item_id=VALUES(quest_item_id), "
-                "quest_item_amount=VALUES(quest_item_amount), quest_target=VALUES(quest_target), "
-                "quest_condition=VALUES(quest_condition), quest_reward=VALUES(quest_reward), "
-                "quest_task=VALUES(quest_task)"
-            )
-            vals = (
-                fort_id, quest_type, time.time(), stardust, pokemon_id, rewardtype, item, itemamount, target,
-                str(condition), str(reward), task
-            )
-            log.debug("{DbWrapperBase::submit_quest_proto} submitted quest typ %s at stop %s" % (
-                str(quest_type), str(fort_id)))
-            self.execute(query_quests, vals, commit=True)
+                task = questtask(int(quest_type), str(condition), int(target))
+
+                query_quests = (
+                    "INSERT into trs_quest (GUID, quest_type, quest_timestamp, quest_stardust, quest_pokemon_id, "
+                    "quest_reward_type, quest_item_id, quest_item_amount, quest_target, quest_condition, quest_reward, "
+                    "quest_task) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    "ON DUPLICATE KEY UPDATE quest_type=VALUES(quest_type), quest_timestamp=VALUES(quest_timestamp), "
+                    "quest_stardust=VALUES(quest_stardust), quest_pokemon_id=VALUES(quest_pokemon_id), "
+                    "quest_reward_type=VALUES(quest_reward_type), quest_item_id=VALUES(quest_item_id), "
+                    "quest_item_amount=VALUES(quest_item_amount), quest_target=VALUES(quest_target), "
+                    "quest_condition=VALUES(quest_condition), quest_reward=VALUES(quest_reward), "
+                    "quest_task=VALUES(quest_task)"
+                )
+                vals = (
+                    fort_id, quest_type, time.time(), stardust, pokemon_id, rewardtype, item, itemamount, target,
+                    str(condition), str(reward), task
+                )
+                log.debug("{DbWrapperBase::submit_quest_proto} submitted quest typ %s at stop %s" % (
+                    str(quest_type), str(fort_id)))
+                self.execute(query_quests, vals, commit=True)
 
             if self.application_args.webhook and self.application_args.quest_webhook:
                 log.debug('Sending quest webhook for pokestop {0}'.format(str(fort_id)))
@@ -892,6 +894,8 @@ class DbWrapperBase(ABC):
             else:
                 log.debug('Sending Webhook is disabled')
 
+        except Exception as e:
+            log.error("Exception on submit_quest_proto : %s" % str(e))
         return True
         
     def create_status_database_if_not_exists(self):
