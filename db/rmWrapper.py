1651e5609a15c8131b3366db3b4622b94dfe8d01
Sascha Ohms <sasch9r@gmail.com>
2019-03-22 09:20:34 +0100

    fix missing import

--- db/rmWrapper.py
+++ db/rmWrapper.py
@@ -6,7 +6,7 @@ import requests
 
 from db.dbWrapperBase import DbWrapperBase
 import logging
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 from utils.collections import Location
 from utils.s2Helper import S2Helper
