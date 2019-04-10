43605013ac674bef093521d8db9196d248b9ee68
cecpk <36961143+cecpk@users.noreply.github.com>
2019-04-10 22:31:05 +0200

    FIX: Mapping error

--- utils/mappingParser.py
+++ utils/mappingParser.py
@@ -204,6 +204,9 @@ class MappingParser(object):
                                                                  pool_arr[pool_settings].get('settings', []))
                         break
                     pool_settings += 1
+             else:
+                 device_dict["settings"] = device.get("settings", None)
+                
             if walker:
                 walker_settings = 0
                 while walker_settings < len(walker_arr):
