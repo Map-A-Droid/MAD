a971676b1e99644c81f44f6b54161bbed41c3c1e
Sascha Ohms <sasch9r@gmail.com>
2019-05-17 15:24:37 +0200

    make logging more configurable

--- utils/walkerArgs.py
+++ utils/walkerArgs.py
@@ -235,25 +235,34 @@ def parseArgs():
                         help='Port of ADB server (Default: 5037)')
 
     # log settings
-    parser.add_argument('--no-file-logs',
-                        help=('Disable logging to files. ' +
-                              'Does not disable --access-logs.'),
-                        action='store_true', default=False)
-    parser.add_argument('--log-path',
-                        help='Defines directory to save log files to.',
-                        default='logs/')
-    parser.add_argument('--log-filename',
-                        help=('Defines the log filename to be saved.'
-                              ' Allows date formatting, and replaces <SN>'
+    parser.add_argument('--no_file_logs', action='store_true', default=False,
+                        help="Disable file logging (Default: file logging is enabled by default)")
+    parser.add_argument('--log_path', default="logs/",
+                        help="Defines directory to save log files to.")
+    parser.add_argument('--log_level',
+                        help=("Forces a certain log level. By default"
+                              " it's set to INFO while being modified"
+                              " by the -v command to show DEBUG logs."
+                              " Custom log levels like DEBUG[1-5] can"
+                              " be used too."))
+    parser.add_argument("--log_file_rotation_size", default="50",
+                        help=("Maximum log file size before rotation creates a"
+                              " new log file. Set to 0 to disable. (Default: 50)"))
+    parser.add_argument("--log_file_level",
+                        help="File logging level. See description for --log_level.")
+    parser.add_argument("--log_file_retention", default="10",
+                        help=("Amount of days to keep file logs. Set to 0 to"
+                              " keep them forever (Default: 10)"))
+    parser.add_argument('--log_filename', default='%Y%m%d_%H%M_<SN>.log', 
+                        help=("Defines the log filename to be saved."
+                              " Allows date formatting, and replaces <SN>"
                               " with the instance's status name. Read the"
-                              ' python time module docs for details.'
-                              ' Default: %%Y%%m%%d_%%H%%M_<SN>.log.'),
-                        default='%Y%m%d_%H%M_<SN>.log')
+                              " python time module docs for details."
+                              " Default: %%Y%%m%%d_%%H%%M_<SN>.log."))
+
     parser.add_argument('-sn', '--status-name', default=str(os.getpid()),
                         help=('Enable status page database update using ' +
                               'STATUS_NAME as main worker name.'))
-    parser.add_argument('-cla', '--cleanup-age', default=0, type=int,
-                        help='Delete logs older than X minutes. Default: 0')
 
     parser.add_argument('-ah', '--auto_hatch', action='store_true', default=False,
                         help='Activate auto hatch of level 5 eggs')
@@ -262,17 +271,20 @@ def parseArgs():
                         help='Auto hatch of level 5 Pokemon ID')
 
     verbose = parser.add_mutually_exclusive_group()
-    verbose.add_argument('-v',
-                         help='Show debug messages',
-                         action='count', default=0, dest='verbose')
+    verbose.add_argument('-v', action='count', default=0, dest='verbose',
+                         help=("Show debug messages. Has no effect, if"
+                               "--log_level has been set."))
+
     verbose.add_argument('--verbosity',
                          help='Show debug messages',
                          type=int, dest='verbose')
     parser.set_defaults(DEBUG=False)
 
     args = parser.parse_args()
+
     # Allow status name and date formatting in log filename.
     args.log_filename = strftime(args.log_filename)
     args.log_filename = args.log_filename.replace('<sn>', '<SN>')
     args.log_filename = args.log_filename.replace('<SN>', args.status_name)
+
     return args
