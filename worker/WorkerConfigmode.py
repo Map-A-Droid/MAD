8ea1a2eead0fc8e8f33dc2bacb3166bfc574923a
cecpk <cec@retrospot.de>
2019-06-23 22:53:48 +0200

    Fix again

--- worker/WorkerConfigmode.py
+++ worker/WorkerConfigmode.py
@@ -1,4 +1,6 @@
 import time
+from threading import Event
+
 from utils.logging import logger
 from websocket.communicator import Communicator
 
@@ -7,6 +9,7 @@ class WorkerConfigmode(object):
     def __init__(self, args, id, websocket_handler):
         self._communicator = Communicator(
             websocket_handler, id, self, args.websocket_command_timeout)
+        self._stop_worker_event = Event()
         self._id = id
 
     def get_communicator(self):
@@ -16,6 +19,8 @@ class WorkerConfigmode(object):
         logger.info("Worker {} started in configmode", str(self._id))
         while not self._stop_worker_event.isSet():
             time.sleep(1)
+        self._communicator.cleanup_websocket()
+        logger.info("Internal cleanup of {} finished", str(self._id))
 
     def stop_worker(self):
         self._stop_worker_event.set()
