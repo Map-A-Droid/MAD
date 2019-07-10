27a9cd2c90af0b98e1cfc61cfcdcb9aace61e09c
cecpk <cec@retrospot.de>
2019-07-10 21:40:40 +0200

    FIX: Trash detection for width 1080

--- ocr/matching_trash.py
+++ ocr/matching_trash.py
@@ -38,9 +38,12 @@ def trash_image_matching(screen_img):
     if trash.mean() == 255 or trash.mean() == 0:
         return clicklist
 
-    if width <= 1080 and width > 720:
+    if width < 1080 and width > 720:
         sc_from = 0.5
         sc_till = 1
+    elif width == 1080:
+        sc_from = 0.5
+        sc_till = 0.8
     elif width == 720:
         sc_from = 0.5
         sc_till = 0.7
@@ -99,4 +102,4 @@ if __name__ == '__main__':
     fort_id = 'raid1'
     fort_img_path = os.getcwd() + '/' + str(fort_id) + '.jpg'
     url_img_path = os.getcwd() + 'ocr/mon_img/ic_raid_egg_rare.png'
-    # print (trash_image_matching('Metallmantel_SamsungS6.jpg'))
+    # print (trash_image_matching('xxxxxxxx.jpg'))
