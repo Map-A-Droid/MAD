82bccc17dc3f3e0914fbdaa734586b2ea06e6e2f
daangroot <groot.d@hotmail.nl>
2019-07-26 15:10:11 +0200

    Remove value from query.

--- db/rmWrapper.py
+++ db/rmWrapper.py
@@ -42,7 +42,7 @@ class RmWrapper(DbWrapperBase):
                 "table": "gym",
                 "column": "is_ex_raid_eligible",
                 "ctype": "tinyint(1) NOT NULL DEFAULT '0'"
-            },       
+            },
             {
                 "table": "pokestop",
                 "column": "incident_start",
@@ -870,7 +870,7 @@ class RmWrapper(DbWrapperBase):
         )
         query_gym_details = (
             "INSERT INTO gymdetails (gym_id, url, last_scanned) "
-            "VALUES (%s, %s, %s, %s) "
+            "VALUES (%s, %s, %s) "
             "ON DUPLICATE KEY UPDATE last_scanned=VALUES(last_scanned), "
             "url=IF(VALUES(url) IS NOT NULL AND VALUES(url) <> '', VALUES(url), url)"
         )
