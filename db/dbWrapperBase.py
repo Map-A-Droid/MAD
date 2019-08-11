b96c2fe3c3e55af1875a429a837c7300fc0f8e2e
JabLuszko <JabLuszko@users.noreply.github.com>
2019-08-11 23:13:37 +0200

    Add check_index_exists

--- db/dbWrapperBase.py
+++ db/dbWrapperBase.py
@@ -42,6 +42,22 @@ class DbWrapperBase(ABC):
                                         pool_size=self.application_args.db_poolsize,
                                         **self.dbconfig)
         self.pool_mutex.release()
+        
+    def check_index_exists(self, table, index):
+        query = (
+            "SELECT count(*) "
+            "FROM information_schema.columns "
+            "WHERE table_name = %s "
+            "AND index_name = %s "
+            "AND table_schema = %s"
+        )
+        vals = (
+            table,
+            index,
+            self.database,
+        )
+
+        return int(self.execute(query, vals)[0][0])
 
     def check_column_exists(self, table, column):
         query = (
