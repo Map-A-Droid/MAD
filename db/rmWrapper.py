dc6777a9ff50841894c461a0d68658fef9f1baea
JabLuszko <JabLuszko@users.noreply.github.com>
2019-08-13 21:37:34 +0200

    order shiny mons by disappear_time
    Newest on top :)

--- db/rmWrapper.py
+++ db/rmWrapper.py
@@ -1790,7 +1790,7 @@ class RmWrapper(DbWrapperBase):
             "trs_stats_detect_raw.worker=b.worker and pokemon.form=a.form), count(DISTINCT encounter_id), a.pokemon_id,"
             "b.worker, GROUP_CONCAT(DISTINCT encounter_id ORDER BY encounter_id DESC SEPARATOR '<br>'), a.form "
             "FROM pokemon a left join trs_stats_detect_raw b on a.encounter_id=CAST(b.type_id as unsigned int) where b.is_shiny=1 group by "
-            "b.is_shiny, a.pokemon_id, a.form, b.worker order by a.pokemon_id"
+            "b.is_shiny, a.pokemon_id, a.form, b.worker order by a.disappear_time DESC"
         )
 
         res = self.execute(query)
