9e7dfc58a027e452a76d6864d28d66eadf0725c8
CECPK <cec@retrospot.de>
2019-09-09 13:54:34 +0200

    reboot if retry window on screen

--- worker/WorkerBase.py
+++ worker/WorkerBase.py
@@ -608,7 +608,7 @@ class WorkerBase(ABC):
                     logger.warning('Something wrong with screendetection')
                     loginerrorcounter += 1
 
-                if loginerrorcounter == 4 or returncode == ScreenType.SN:
+                if loginerrorcounter == 4 or returncode in (ScreenType.SN, ScreenType.RETRY):
                     logger.error('Cannot login again - (clear pogo game data and) restart phone / SN Error')
                     self._stop_worker_event.set()
                     self._stop_pogo()
