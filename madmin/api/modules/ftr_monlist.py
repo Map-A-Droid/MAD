0cc949f6a0b33b86d73261a92da73640556c5809
Chris <chris.dohmen11@gmail.com>
2019-09-11 12:14:21 +0000

    Moved monlist vars into the configuration

--- madmin/api/modules/ftr_monlist.py
+++ madmin/api/modules/ftr_monlist.py
@@ -5,6 +5,31 @@ class APIMonList(apiHandler.ResourceHandler):
     component = 'monlist'
     description = 'Add/Update/Delete Pokemon Lists (honestly i have no idea)'
 
+    configuration = {
+        "monivlist": {
+            "fields": {
+                "monlist": {
+                    "settings": {
+                        "type": "text",
+                        "require": "true",
+                        "empty": "null",
+                        "description": "Name of global Monlist",
+                        "lockonedit": "true"
+                    }
+                },
+                "mon_ids_iv": {
+                    "settings": {
+                        "type": "text",
+                        "require": "false",
+                        "description": "Encounter these mon ids while walking (Put in brackets [1,2,3] as comma separated list!)",
+                        "output": "int",
+                        "showmonsidpicker": true
+                    }
+                }
+            }
+        }
+    }
+
     def validate_dependencies(self):
         # TODO - Figure out requirements
         return True
