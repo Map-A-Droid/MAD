33619ff897413091c3362d546c8d5e9020c8c731
Chris <chris.dohmen11@gmail.com>
2019-09-13 17:02:53 +0000

    Updates to the area configuration to display the same information as the old UI

--- madmin/api/modules/ftr_area.py
+++ madmin/api/modules/ftr_area.py
@@ -68,7 +68,7 @@ class APIArea(apiHandler.ResourceHandler):
                 "speed": {
                     "settings": {
                         "type": "text",
-                        "require": True,
+                        "require": False,
                         "description": "Speed of player in kmh",
                         "output": int,
                         "expected": float
@@ -77,7 +77,7 @@ class APIArea(apiHandler.ResourceHandler):
                 "max_distance": {
                     "settings": {
                         "type": "text",
-                        "require": True,
+                        "require": False,
                         "description": "Max. distance of walking - otherwise teleport to new location",
                         "output": int,
                         "expected": float
@@ -192,7 +192,7 @@ class APIArea(apiHandler.ResourceHandler):
                 "speed": {
                     "settings": {
                         "type": "text",
-                        "require": True,
+                        "require": False,
                         "description": "Speed of player in kmh",
                         "output": int,
                         "expected": float
@@ -201,7 +201,7 @@ class APIArea(apiHandler.ResourceHandler):
                 "max_distance": {
                     "settings": {
                         "type": "text",
-                        "require": True,
+                        "require": False,
                         "description": "Max. distance of walking - otherwise teleport to new location",
                         "output": int,
                         "expected": float
@@ -324,7 +324,7 @@ class APIArea(apiHandler.ResourceHandler):
                 "route_calc_algorithm": {
                     "settings": {
                         "type": "option",
-                        "values": "optimized|quick",
+                        "values": ['optimized','quick'],
                         "require": False,
                         "description": "Method of calculation for routes. (Default optimized)",
                         "expected": str
@@ -335,7 +335,7 @@ class APIArea(apiHandler.ResourceHandler):
                 "speed": {
                     "settings": {
                         "type": "text",
-                        "require": True,
+                        "require": False,
                         "description": "Speed of player in kmh",
                         "output": int,
                         "expected": float
@@ -344,7 +344,7 @@ class APIArea(apiHandler.ResourceHandler):
                 "max_distance": {
                     "settings": {
                         "type": "text",
-                        "require": True,
+                        "require": False,
                         "description": "Max. distance of walking - otherwise teleport to new location",
                         "output": int,
                         "expected": float
@@ -354,7 +354,7 @@ class APIArea(apiHandler.ResourceHandler):
                     "settings": {
                         "type": "option",
                         "values": [True, False],
-                        "require": True,
+                        "require": False,
                         "description": "Do not spinn stops already made in the past (for levelmode)",
                         "output": int,
                         "expected": bool
@@ -364,7 +364,7 @@ class APIArea(apiHandler.ResourceHandler):
                     "settings": {
                         "type": "option",
                         "values": [False, True],
-                        "require": True,
+                        "require": False,
                         "description": "Cleanup quest inventory every spinned stop",
                         "output": int,
                         "expected": bool
@@ -418,13 +418,22 @@ class APIArea(apiHandler.ResourceHandler):
                         "description": "Name of routefile",
                         "expected": str
                     }
+                },
+                "including_stops": {
+                    "settings": {
+                        "type": "option",
+                        "require": True,
+                        "values": [False, True],
+                        "description": "Calculate route including stops to catch invasions.",
+                        "expected": bool
+                    }
                 }
             },
             "settings": {
                 "speed": {
                     "settings": {
                         "type": "text",
-                        "require": True,
+                        "require": False,
                         "description": "Speed of player in kmh",
                         "expected": float
                     }
@@ -432,7 +441,7 @@ class APIArea(apiHandler.ResourceHandler):
                 "max_distance": {
                     "settings": {
                         "type": "text",
-                        "require": True,
+                        "require": False,
                         "description": "Max. distance of walking - otherwise teleport to new location",
                         "expected": float
                     }
@@ -524,100 +533,100 @@ class APIArea(apiHandler.ResourceHandler):
                     }
                 }
             }
-        },
-        "raids_ocr": {
-            "description": "OCR scanner for detecting raids.",
-            "fields": {
-                "name": {
-                    "settings": {
-                        "type": "text",
-                        "require": True,
-                        "description": "Name of area",
-                        "lockonedit": True,
-                        "expected": str
-                    }
-                },
-                "geofence_included": {
-                    "settings": {
-                        "type": "text",
-                        "require": True,
-                        "empty": None,
-                        "description": "Including geofence for scanarea",
-                        "expected": str
-                    }
-                },
-                "geofence_excluded": {
-                    "settings": {
-                        "type": "text",
-                        "require": False,
-                        "empty": None,
-                        "description": "Excluding geofence for scanarea",
-                        "expected": str
-                    }
-                },
-                "routecalc": {
-                    "settings": {
-                        "type": "text",
-                        "require": True,
-                        "description": "Name of routefile",
-                        "expected": str
-                    }
-                }
-            },
-            "settings": {
-                "speed": {
-                    "settings": {
-                        "type": "text",
-                        "require": True,
-                        "description": "Speed of player in kmh",
-                        "expected": float
-                    }
-                },
-                "max_distance": {
-                    "settings": {
-                        "type": "text",
-                        "require": True,
-                        "description": "Max. distance of walking - otherwise teleport to new location",
-                        "expected": float
-                    }
-                },
-                "delay_after_prio_event": {
-                    "settings": {
-                        "type": "text",
-                        "require": False,
-                        "description": "Offset to be added to events such as spawns or raid starts. E.g. if you want to scan gyms at least a minute after an egg has hatched, set it to 60 (Default: empty)<br>Empty = Disable PrioQ",
-                        "output": int,
-                        "expected": int
-                    }
-                },
-                "priority_queue_clustering_timedelta": {
-                    "settings": {
-                        "type": "text",
-                        "require": False,
-                        "description": "Cluster events within the given timedelta in seconds. The latest event in time within a timedelta will be used to scan the clustered events (Default: 0)",
-                        "output": int,
-                        "expected": float
-                    }
-                },
-                "remove_from_queue_backlog": {
-                    "settings": {
-                        "type": "text",
-                        "require": False,
-                        "description": "Remove any events in priority queue that have been due for scanning before NOW - given time in seconds (Default: 0)",
-                        "output": int,
-                        "expected": float
-                    }
-                },
-                "starve_route": {
-                    "settings": {
-                        "type": "option",
-                        "require": False,
-                        "values": [False, True],
-                        "description": "Disable round-robin of route vs. priority queue events. If True,    your route may not be completed in time and e.g. only spawns will be scanned",
-                        "output": int,
-                        "expected": bool
-                    }
-                }
-            }
         }
+        # "raids_ocr": {
+        #     "description": "OCR scanner for detecting raids.",
+        #     "fields": {
+        #         "name": {
+        #             "settings": {
+        #                 "type": "text",
+        #                 "require": True,
+        #                 "description": "Name of area",
+        #                 "lockonedit": True,
+        #                 "expected": str
+        #             }
+        #         },
+        #         "geofence_included": {
+        #             "settings": {
+        #                 "type": "text",
+        #                 "require": True,
+        #                 "empty": None,
+        #                 "description": "Including geofence for scanarea",
+        #                 "expected": str
+        #             }
+        #         },
+        #         "geofence_excluded": {
+        #             "settings": {
+        #                 "type": "text",
+        #                 "require": False,
+        #                 "empty": None,
+        #                 "description": "Excluding geofence for scanarea",
+        #                 "expected": str
+        #             }
+        #         },
+        #         "routecalc": {
+        #             "settings": {
+        #                 "type": "text",
+        #                 "require": True,
+        #                 "description": "Name of routefile",
+        #                 "expected": str
+        #             }
+        #         }
+        #     },
+        #     "settings": {
+        #         "speed": {
+        #             "settings": {
+        #                 "type": "text",
+        #                 "require": True,
+        #                 "description": "Speed of player in kmh",
+        #                 "expected": float
+        #             }
+        #         },
+        #         "max_distance": {
+        #             "settings": {
+        #                 "type": "text",
+        #                 "require": True,
+        #                 "description": "Max. distance of walking - otherwise teleport to new location",
+        #                 "expected": float
+        #             }
+        #         },
+        #         "delay_after_prio_event": {
+        #             "settings": {
+        #                 "type": "text",
+        #                 "require": False,
+        #                 "description": "Offset to be added to events such as spawns or raid starts. E.g. if you want to scan gyms at least a minute after an egg has hatched, set it to 60 (Default: empty)<br>Empty = Disable PrioQ",
+        #                 "output": int,
+        #                 "expected": int
+        #             }
+        #         },
+        #         "priority_queue_clustering_timedelta": {
+        #             "settings": {
+        #                 "type": "text",
+        #                 "require": False,
+        #                 "description": "Cluster events within the given timedelta in seconds. The latest event in time within a timedelta will be used to scan the clustered events (Default: 0)",
+        #                 "output": int,
+        #                 "expected": float
+        #             }
+        #         },
+        #         "remove_from_queue_backlog": {
+        #             "settings": {
+        #                 "type": "text",
+        #                 "require": False,
+        #                 "description": "Remove any events in priority queue that have been due for scanning before NOW - given time in seconds (Default: 0)",
+        #                 "output": int,
+        #                 "expected": float
+        #             }
+        #         },
+        #         "starve_route": {
+        #             "settings": {
+        #                 "type": "option",
+        #                 "require": False,
+        #                 "values": [False, True],
+        #                 "description": "Disable round-robin of route vs. priority queue events. If True,    your route may not be completed in time and e.g. only spawns will be scanned",
+        #                 "output": int,
+        #                 "expected": bool
+        #             }
+        #         }
+        #     }
+        # }
     }
