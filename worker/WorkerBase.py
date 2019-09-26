b29af52ddbf92fc1b3cf00c49533b3f6f5d0ee19
Isdor <chrisboards@gmx.de>
2019-09-26 18:00:36 +0200

    Added magisk toggle

--- worker/WorkerBase.py
+++ worker/WorkerBase.py
@@ -636,13 +636,18 @@ class WorkerBase(ABC):
                     self._reboot()
 
                 if loginerrorcounter == 2:
-                    logger.error('Cannot login two times in row - restart pogo')
-                    self._stop_pogo()
-                    time.sleep(5)
-                    self._communicator.clearAppCache("com.nianticlabs.pokemongo")
-                    self._turn_screen_on_and_start_pogo()
 
-                self._last_screen_type = returncode
+                    logger.error('Cannot login two times in row - toggling magiskhide on/off and restarting pogo')
+                     self._stop_pogo()
+                     time.sleep(5)
+                     self._communicator.magisk_off("com.nianticlabs.pokemongo")
+                     self._communicator.clearAppCache("com.nianticlabs.pokemongo")
+                     time.sleep(1)
+                     self._communicator.magisk_on("com.nianticlabs.pokemongo")
+                     time.sleep(1)
+                     self._reboot()
+
+               self._last_screen_type = returncode
 
         logger.info('Checking pogo screen is finished')
         return True
