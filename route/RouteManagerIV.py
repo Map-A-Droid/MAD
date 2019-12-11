7537beaad9cfe68e510c6c7f28843a573137bf0c
Chris Dohmen <chris.dohmen11@gmail.com>
2019-12-11 09:36:32 -0500

    QuestRoute Fix
    Quest routes were not being regenerated to pickup new stops

--- route/RouteManagerIV.py
+++ route/RouteManagerIV.py
@@ -13,7 +13,7 @@ class RouteManagerIV(RouteManagerBase):
         return True
 
     def _recalc_route_workertype(self):
-        self.recalc_route(self._max_radius, self._max_coords_within_radius, 1, delete_old_route=False)
+        self.recalc_route(self._max_radius, self._max_coords_within_radius, 1, delete_old_route=False, in_memory=False)
 
     def _retrieve_latest_priority_queue(self):
         # IV is excluded from clustering, check RouteManagerBase for more info
