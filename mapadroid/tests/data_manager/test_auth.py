0b869596f3ca233500ba8c10939d53ce8696b1ce
Sascha Ohms <sasch9r@gmail.com>
2020-01-17 12:47:52 +0100

    remove unused tests

--- mapadroid/tests/data_manager/test_auth.py
+++
@@ -1,9 +0,0 @@
-from mapadroid.tests.data_manager.base import DataManagerBase
-from mapadroid.utils.data_manager.modules.auth import Auth
-
-
-class DMAuth(DataManagerBase):
-    base_resource = Auth
-
-    def test_echo(self):
-        self.assertTrue(False)
