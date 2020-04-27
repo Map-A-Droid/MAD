41a045b77f0721fc8d377e88226f65ab6df59180
cec <{ID}+{username}@users.noreply.github.com>
2020-04-27 20:44:47 +0200

    FIX: delete shiny raw stats

--- mapadroid/db/DbStatsSubmit.py
+++ mapadroid/db/DbStatsSubmit.py
@@ -67,6 +67,6 @@ class DbStatsSubmit:
         if self._args.raw_delete_shiny > 0:
             query = (
                     "DELETE FROM trs_stats_detect_raw WHERE timestamp_scan < "
-                    "(UNIX_TIMESTAMP() - " + str(self._args.raw_delete_shiny * 86400) + ") AND is_shiny = 1"
+                    "(UNIX_TIMESTAMP() - " + str(int(self._args.raw_delete_shiny) * 86400) + ") AND is_shiny = 1"
             )
             self._db_exec.execute(query, commit=True)
