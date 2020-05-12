f2499b788d68bf6e6dcb13b5af64653fffe8b698
cec <{ID}+{username}@users.noreply.github.com>
2020-05-12 09:00:23 +0200

    Revert "Moved GPlayConnector to its own file.  Implemented Token Dispenser"
    This reverts commit c346e605

--- mapadroid/utils/walkerArgs.py
+++ mapadroid/utils/walkerArgs.py
@@ -23,13 +23,9 @@ def memoize(function):
 @memoize
 def parseArgs():
     defaultconfigfiles = []
-    default_tokenfile = None
     if '-cf' not in sys.argv and '--config' not in sys.argv:
         defaultconfigfiles = [os.getenv('MAD_CONFIG', os.path.join(
             mapadroid.MAD_ROOT, 'configs/config.ini'))]
-    if '-td' not in sys.argv and '--token_dispenser' not in sys.argv:
-        default_tokenfile = os.getenv('MAD_CONFIG', os.path.join(
-            mapadroid.MAD_ROOT, 'configs/token-dispensers.ini'))
     parser = configargparse.ArgParser(
         default_config_files=defaultconfigfiles,
         auto_env_var_prefix='THERAIDMAPPER_')
@@ -351,10 +347,6 @@ def parseArgs():
                         help='Run unit tests then quit', dest='unit_tests')
 
     # MADAPKs
-    parser.add_argument('-td', '--token_dispenser', default=default_tokenfile,
-                        help='Token dispenser config (MAD)')
-    parser.add_argument('-tdu', '--token_dispenser_user', default='',
-                        help='Token dispenser config (User)')
     parser.add_argument('-gu', '--gmail_user', default='',
                         help='Google Mail User for interacting with the Google Play Store')
     parser.add_argument('-gp', '--gmail_passwd', default='',
