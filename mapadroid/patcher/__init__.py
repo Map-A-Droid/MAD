4a986f6ca0b618fd49eae348c65a3642d7a6cb79
Sascha Ohms <sasch9r@gmail.com>
2020-06-09 16:03:34 +0200

    switch order of patches to prevent issues

--- mapadroid/patcher/__init__.py
+++ mapadroid/patcher/__init__.py
@@ -33,8 +33,8 @@ MAD_UPDATES = OrderedDict([
     (29, 'patch_29'),
     (30, 'patch_30'),
     (31, 'trs_status_lastProtoDateTime_fix'),
-    (32, 'routecalc_rename'),
-    (33, 'reset_routecalc_algo')
+    (32, 'reset_routecalc_algo'),
+    (33, 'routecalc_rename')
 ])
 
 
