8c62f744a1b6c575942af9436a513463fe683668
Sascha Ohms <sasch9r@gmail.com>
2020-06-09 18:23:57 +0200

    only reset non-routefree areas

--- mapadroid/patcher/reset_routecalc_algo.py
+++ mapadroid/patcher/reset_routecalc_algo.py
@@ -6,9 +6,14 @@ class Patch(PatchBase):
     descr = 'Resets the routecalc algorithm for all pokestop routes to fallback to "quick".'
 
     def _execute(self):
-        origin_sql = "UPDATE settings_area_pokestops SET route_calc_algorithm=NULL"
+        sql = (
+            "UPDATE settings_area_pokestops "
+            "SET route_calc_algorithm=NULL "
+            "WHERE route_calc_algorithm != 'routefree'"
+        )
+
         try:
-            self._db.execute(origin_sql, commit=True, raise_exec=True)
+            self._db.execute(sql, commit=True, raise_exec=True)
         except Exception as e:
             self._logger.exception("Unexpected error: {}", e)
             self.issues = True
