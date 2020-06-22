b8f2b9a9385706fb49baecebdbeceb56e443c09c
JabLuszko <you@example.com>
2020-06-22 22:46:10 +0200

    Cleanup

--- static/madmin/static/js/get_device_note.js
+++ static/madmin/static/js/get_device_note.js
@@ -1,28 +1,21 @@
 $(document).ready(function() {
-
-    console.log("get_device_note ready!");
-
     if (jQuery.ui) {
-        console.log("jQuery UI loaded nothing to do");
+        // # console.log("jQuery UI loaded nothing to do");
     } else {
         var js = document.createElement("script");
         js.type = "text/javascript";
         js.src = "https://code.jquery.com/ui/1.12.1/jquery-ui.js";
         $("head").append(js);
         $('head').append('<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" type="text/css" />');
-        console.log("Added jQuery UI");
+        // # console.log("Added jQuery UI");
     }
-
     $(".device_note").bind("click", function() {
         deviceid = $(this).data('deviceid');
         click_device_notes(deviceid);
     });
-
-
 });
 
 function click_device_notes(deviceid) {
-    console.log("HAAAAAAAALO");
     $.ajax({
         type: 'GET',
         url:'get_device_note',
