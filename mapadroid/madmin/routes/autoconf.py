f2de8d4e93b276ca5c993de9b7e74e6a1d17f7da
Chris Dohmen <chris.dohmen11@gmail.com>
2020-07-29 08:07:57 -0400

    Added log viewer page for autoconfig session

--- mapadroid/madmin/routes/autoconf.py
+++ mapadroid/madmin/routes/autoconf.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import jsonify, render_template, redirect, url_for
 from mapadroid.madmin.functions import auth_required
 from mapadroid.utils.autoconfig import validate_hopper_ready, RGCConfig, PDConfig
 
@@ -15,6 +15,8 @@ class AutoConfigManager(object):
             ("/autoconfig", self.autoconfig_root),
             ("/autoconfig/pending", self.autoconfig_pending),
             ("/autoconfig/pending/<int:session_id>", self.autoconfig_pending_dev),
+            ("/autoconfig/logs/<int:session_id>", self.autoconf_logs),
+            ("/autoconfig/logs/<int:session_id>/update", self.autoconf_logs_get),
             ("/autoconfig/rgc", self.autoconf_rgc),
             ("/autoconfig/pd", self.autoconf_pd),
             ("/autoconfig/google", self.autoconf_google),
@@ -64,6 +66,35 @@ class AutoConfigManager(object):
                                redirect=redir_uri,
                                method='POST'
                                )
+
+    @auth_required
+    def autoconf_logs(self, session_id):
+        sql = "SELECT *\n"\
+              "FROM `autoconfig_registration`\n"\
+              "WHERE `session_id` = %s AND `instance_id` = %s"
+        session = self._db.autofetch_row(sql, (session_id, self._db.instance_id))
+        if not session:
+            return redirect(url_for('autoconfig_pending'), code=302)
+        return render_template('autoconfig_logs.html',
+                               subtab="autoconf_logs",
+                               responsive=str(self._args.madmin_noresponsive).lower(),
+                               session_id=session_id
+                               )
+
+    @auth_required
+    def autoconf_logs_get(self, session_id):
+        sql = "SELECT *\n"\
+              "FROM `autoconfig_registration`\n"\
+              "WHERE `session_id` = %s AND `instance_id` = %s"
+        session = self._db.autofetch_row(sql, (session_id, self._db.instance_id))
+        if not session:
+            return redirect(url_for('autoconfig_pending'), code=302)
+        sql = "SELECT UNIX_TIMESTAMP(`log_time`) as 'log_time', `level`, `msg`\n"\
+              "FROM `autoconfig_logs`\n"\
+              "WHERE `instance_id` = %s AND `session_id` = %s\n"\
+              "ORDER BY `log_time` ASC"
+        logs = self._db.autofetch_all(sql, (self._db.instance_id, session_id))
+        return jsonify(logs)
 
     @auth_required
     def autoconf_pd(self):
