8d882378e81d5dba644faf2a5b82b3d77884aa25
JabLuszko <JabLuszko@users.noreply.github.com>
2020-08-04 02:27:45 +0200

    Fix variable name in pluginBase.py to be less generic (#944)

--- mapadroid/utils/pluginBase.py
+++ mapadroid/utils/pluginBase.py
@@ -128,9 +128,9 @@ class PluginCollection(object):
         rootlen = len(folder) + 1
         for base, _, files in os.walk(folder):
             if "__pycache__" not in base:
-                for file in files:
-                    if file != "plugin.ini":
-                        fn = os.path.join(base, file)
+                for file_to_zip in files:
+                    if file_to_zip != "plugin.ini":
+                        fn = os.path.join(base, file_to_zip)
                         zipobj.write(fn, fn[rootlen:])
 
         zipobj.close()
