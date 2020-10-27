8a7c1e1a3ea7f56f8cbefc13ffde2d8c10b825e5
fosJoddie <5710881+fosJoddie@users.noreply.github.com>
2020-10-27 19:47:45 +0100

    Simplify mitm dataprocessor logic

--- mapadroid/mitm_receiver/SerializedMitmDataProcessor.py
+++ mapadroid/mitm_receiver/SerializedMitmDataProcessor.py
@@ -38,46 +38,49 @@ class SerializedMitmDataProcessor(Process):
         origin_logger.debug2("Processing received data")
         processed_timestamp = datetime.fromtimestamp(received_timestamp)
 
-        if data_type and not data.get("raw", False):
-            self.__mitm_mapper.run_stats_collector(origin)
+        if not data_type or data.get("raw", False):
+            return
 
-            origin_logger.debug4("Received data: {}", data)
-            if data_type == 106:
-                # process GetMapObject
-                origin_logger.info("Processing GMO received. Received at {}", processed_timestamp)
+        self.__mitm_mapper.run_stats_collector(origin)
 
-                self.__db_submit.weather(origin, data["payload"], received_timestamp)
+        origin_logger.debug4("Received data: {}", data)
+        data_payload = data["payload"]
+        if data_type == 106:
+            # process GetMapObject
+            origin_logger.info("Processing GMO received. Received at {}", processed_timestamp)
 
-                self.__db_submit.stops(origin, data["payload"])
-                self.__db_submit.gyms(origin, data["payload"])
-                self.__db_submit.raids(origin, data["payload"], self.__mitm_mapper)
+            self.__db_submit.weather(origin, data_payload, received_timestamp)
 
-                self.__db_submit.spawnpoints(origin, data["payload"], processed_timestamp)
-                self.__db_submit.mons(origin, received_timestamp, data["payload"], self.__mitm_mapper)
-                self.__db_submit.cells(origin, data["payload"])
-                self.__mitm_mapper.submit_gmo_for_location(origin, data["payload"])
-                origin_logger.debug2("Done processing GMO")
-            elif data_type == 102:
-                playerlevel = self.__mitm_mapper.get_playerlevel(origin)
-                if playerlevel >= 30:
-                    origin_logger.debug("Processing encounter received at {}", processed_timestamp)
-                    self.__db_submit.mon_iv(origin, received_timestamp, data["payload"], self.__mitm_mapper)
-                    origin_logger.debug2("Done processing encounter")
-                else:
-                    origin_logger.warning("Playerlevel lower than 30 - not processing encounter IVs")
-            elif data_type == 101:
-                origin_logger.debug2("Processing proto 101")
-                self.__db_submit.quest(origin, data["payload"], self.__mitm_mapper)
-                origin_logger.debug2("Done processing proto 101")
-            elif data_type == 104:
-                origin_logger.debug2("Processing proto 104")
-                self.__db_submit.stop_details(data["payload"])
-                origin_logger.debug2("Done processing proto 104")
-            elif data_type == 4:
-                origin_logger.debug2("Processing proto 4")
-                self.__mitm_mapper.generate_player_stats(origin, data["payload"])
-                origin_logger.debug2("Done processing proto 4")
-            elif data_type == 156:
-                origin_logger.debug2("Processing proto 156")
-                self.__db_submit.gym(origin, data["payload"])
-                origin_logger.debug2("Done processing proto 156")
+            self.__db_submit.stops(origin, data_payload)
+            self.__db_submit.gyms(origin, data_payload)
+            self.__db_submit.raids(origin, data_payload, self.__mitm_mapper)
+
+            self.__db_submit.spawnpoints(origin, data_payload, processed_timestamp)
+            self.__db_submit.mons(origin, received_timestamp, data_payload, self.__mitm_mapper)
+            self.__db_submit.cells(origin, data_payload)
+            self.__mitm_mapper.submit_gmo_for_location(origin, data_payload)
+            origin_logger.debug2("Done processing GMO")
+        elif data_type == 102:
+            playerlevel = self.__mitm_mapper.get_playerlevel(origin)
+            if playerlevel >= 30:
+                origin_logger.debug("Processing encounter received at {}", processed_timestamp)
+                self.__db_submit.mon_iv(origin, received_timestamp, data_payload, self.__mitm_mapper)
+                origin_logger.debug2("Done processing encounter")
+            else:
+                origin_logger.warning("Playerlevel lower than 30 - not processing encounter IVs")
+        elif data_type == 101:
+            origin_logger.debug2("Processing proto 101")
+            self.__db_submit.quest(origin, data_payload, self.__mitm_mapper)
+            origin_logger.debug2("Done processing proto 101")
+        elif data_type == 104:
+            origin_logger.debug2("Processing proto 104")
+            self.__db_submit.stop_details(data_payload)
+            origin_logger.debug2("Done processing proto 104")
+        elif data_type == 4:
+            origin_logger.debug2("Processing proto 4")
+            self.__mitm_mapper.generate_player_stats(origin, data_payload)
+            origin_logger.debug2("Done processing proto 4")
+        elif data_type == 156:
+            origin_logger.debug2("Processing proto 156")
+            self.__db_submit.gym(origin, data_payload)
+            origin_logger.debug2("Done processing proto 156")
