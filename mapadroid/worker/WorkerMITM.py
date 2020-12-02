fafa6c637f37af42825ad78ce94cf4e264b379b8
Till Skrodzki <till@mueskro.de>
2020-12-02 13:30:36 +0100

    Fix wrong comparison against a tuple in WorkerMITM.py

--- mapadroid/worker/WorkerMITM.py
+++ mapadroid/worker/WorkerMITM.py
@@ -43,7 +43,8 @@ class WorkerMITM(MITMBase):
 
     def _post_move_location_routine(self, timestamp):
         # TODO: pass the appropiate proto number if IV?
-        if self._wait_for_data(timestamp) != LatestReceivedType.GMO:
+        type_received, data = self._wait_for_data(timestamp)
+        if type_received != LatestReceivedType.GMO:
             self.logger.warning("Worker failed to retrieve proper data at {}, {}. Worker will continue with "
                                 "the next location", self.current_location.lat, self.current_location.lng)
 
