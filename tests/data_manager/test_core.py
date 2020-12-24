e577f82bfaff7903ee90fb3f83434fc9a6de830f
Expl0dingBanana <chris.dohmen11@gmail.com>
2020-12-24 09:29:20 -0500

    Updates to tox testing
     * Added a few data_manager tests
     * Added fixtures for mocked DB
     * Added tox-gh-actions

--- tests/data_manager/test_core.py
+++ tests/data_manager/test_core.py
@@ -1,6 +1,9 @@
+from unittest.mock import call
+
 import pytest
 
 from mapadroid.data_manager import dm_exceptions, modules
+from mapadroid.data_manager.modules.auth import Auth
 
 
 def test_get_resource_class(data_manager):
@@ -23,4 +26,33 @@ def test_get_resource_class(data_manager):
 
 def test_get_resource_invalid(data_manager):
     with pytest.raises(dm_exceptions.UnknownIdentifier):
+        data_manager.dbc.autofetch_row.return_value = None
         data_manager.get_resource("device", 1)
+    with pytest.raises(dm_exceptions.UnknownIdentifier):
+        data_manager.dbc.autofetch_row.return_value = None
+        data_manager.get_resource("area", 1)
+
+
+def test_clear_on_boot(data_manager):
+    data_manager.clear_on_boot()
+    assert data_manager.dbc.mock_calls[0] == call.autoexec_update('settings_routecalc', {'recalc_status': 0},
+                                                                  where_keyvals={'instance_id': 1})
+    data_manager.instance_id = 1234
+    data_manager.clear_on_boot()
+    assert data_manager.dbc.mock_calls[1] == call.autoexec_update('settings_routecalc', {'recalc_status': 0},
+                                                                  where_keyvals={'instance_id': 1234})
+
+
+def test_save_resource(data_manager):
+    resource = Auth(data_manager)
+    resource["username"] = "test"
+    resource["password"] = "pass"
+    resource.save()
+    assert data_manager.dbc.mock_calls[-1] == call.autoexec_insert("settings_auth", {'username': 'test',
+                                                                                     'password': 'pass',
+                                                                                     'instance_id': 1})
+    resource = Auth(data_manager)
+    resource["username"] = "test"
+    with pytest.raises(dm_exceptions.UpdateIssue) as issues:
+        resource.save()
+    assert issues.value.issues == {'missing': ['password']}
