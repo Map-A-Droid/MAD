041c9b6a110536d5da72f36d7b838310341cb372
JabLuszko <JabLuszko@users.noreply.github.com>
2021-01-18 18:15:00 +0100

    Fix PogoDroid URL from .de to .eu
    It's broken now :(

--- mapadroid/utils/global_variables.py
+++ mapadroid/utils/global_variables.py
@@ -4,6 +4,6 @@
 CHUNK_MAX_SIZE = 1024 * 1024 * 8  # 8MiB
 MAD_APK_ALLOWED_EXTENSIONS = set(['apk', 'zip'])
 URL_RGC_APK = 'https://raw.githubusercontent.com/Map-A-Droid/MAD/master/APK/RemoteGpsController.apk'
-URL_PD_APK = 'https://www.maddev.de/apk/PogoDroid.apk'
+URL_PD_APK = 'https://www.maddev.eu/apk/PogoDroid.apk'
 
 VERSIONCODES_URL = 'https://raw.githubusercontent.com/Map-A-Droid/MAD/master/configs/version_codes.json'
