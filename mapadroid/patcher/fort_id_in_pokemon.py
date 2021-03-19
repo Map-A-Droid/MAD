2f8872ff2c3a8672617495d93c8577a0b995a960
ccev <42342921+ccev@users.noreply.github.com>
2021-03-19 19:46:36 +0100

    database updates

---
+++ mapadroid/patcher/fort_id_in_pokemon.py
@@ -0,0 +1,18 @@
+from ._patch_base import PatchBase
+
+
+class Patch(PatchBase):
+    name = 'Add fort_id column to pokemon table'
+    descr = 'Used for nearby scanning'
+
+    def _execute(self):
+        alter_pokemon = """
+            ALTER TABLE `pokemon`
+            ADD COLUMN `fort_id` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
+        """
+        try:
+            if not self._schema_updater.check_column_exists("pokemon", "fort_id"):
+                self._db.execute(alter_pokemon, commit=True, raise_exec=True)
+        except Exception as e:
+            self._logger.exception("Unexpected error: {}", e)
+            self.issues = True
