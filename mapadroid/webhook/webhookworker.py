000557167289c3cbcb55ba7de736eb7b98da8611
ccev <42342921+ccev@users.noreply.github.com>
2021-04-13 15:29:13 +0200

    ignore nonivs arg

--- mapadroid/webhook/webhookworker.py
+++ mapadroid/webhook/webhookworker.py
@@ -414,12 +414,14 @@ class WebhookWorker:
             if self.__is_in_excluded_area([mon["latitude"], mon["longitude"]]):
                 continue
 
+            """
             if not self.__args.pokemon_webhook_nonivs \
                and mon["pokemon_id"] in self.__IV_MON \
                and (mon["individual_attack"] is None) \
                and (mon["fort_id"] is None):
                 # skipping this mon since IV has not been scanned yet
                 continue
+            """
 
             mon_payload = {
                 "encounter_id": mon["encounter_id"],
