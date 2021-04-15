d5704f1df5bde11d40c316316ae81c0409b1e0bd
ccev <42342921+ccev@users.noreply.github.com>
2021-04-15 18:05:06 +0200

    make pvp plugin work again

--- mapadroid/webhook/webhookworker.py
+++ mapadroid/webhook/webhookworker.py
@@ -79,10 +79,8 @@ class WebhookWorker:
 
             if sub_types is not None:
                 for payload in payloads:
-                    if payload["type"] in sub_types:
-                        if payload["type"] in self.__valid_mon_types:
-                            payload = payload.copy()
-                            payload["type"] = "pokemon"
+                    if payload["type"] in sub_types or \
+                       (payload["message"].get("seen_type") in sub_types):
                         payload_to_send.append(payload)
             else:
                 payload_to_send = payloads
@@ -495,7 +493,7 @@ class WebhookWorker:
                 )
                 mon_payload["cell_id"] = mon["cell_id"]
 
-            entire_payload = {"type": str(mon["seen_type"]), "message": mon_payload}
+            entire_payload = {"type": "pokemon", "message": mon_payload}
             ret.append(entire_payload)
 
         return ret
