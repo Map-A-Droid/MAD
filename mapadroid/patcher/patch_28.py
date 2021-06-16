83b282cb1903bd0508fee7e5c2a93a94e4e6b0d0
JabLuszko <JabLuszko@users.noreply.github.com>
2021-06-16 12:58:40 +0200

    Update patch_28.py

--- mapadroid/patcher/patch_28.py
+++ mapadroid/patcher/patch_28.py
@@ -14,7 +14,7 @@ class Patch(PatchBase):
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
         """
         try:
-            self._db.execute(origin_sql, commit=True, raise_exec=True)
+            self._db.execute(origin_sql, commit=True, raise_exc=True)
         except Exception as e:
             self._logger.exception("Unexpected error: {}", e)
             self.issues = True
