9183ba907328debc5600166c805452b30710b59c
Grennith <till@mueskro.de>
2021-06-16 08:16:45 +0200

    WIP: Pass walker_id for settings_walkerarea

--- mapadroid/madmin/endpoints/api/resources/WalkerareaEndpoint.py
+++ mapadroid/madmin/endpoints/api/resources/WalkerareaEndpoint.py
@@ -1,8 +1,9 @@
-from typing import Dict, Optional, Set
+from typing import Dict, Optional, Set, List
 
+from mapadroid.db.helper.SettingsWalkerToWalkerareaHelper import SettingsWalkerToWalkerareaHelper
 from mapadroid.db.helper.SettingsWalkerareaHelper import \
     SettingsWalkerareaHelper
-from mapadroid.db.model import (Base, SettingsWalkerarea)
+from mapadroid.db.model import (Base, SettingsWalkerarea, SettingsWalkerToWalkerarea)
 from mapadroid.db.resource_definitions.Walkerarea import Walkerarea
 from mapadroid.madmin.endpoints.api.resources.AbstractResourceEndpoint import \
     AbstractResourceEndpoint
@@ -28,4 +29,25 @@ class WalkerareaEndpoint(AbstractResourceEndpoint):
         walkerarea: SettingsWalkerarea = SettingsWalkerarea()
         walkerarea.instance_id = self._get_instance_id()
         walkerarea.walkerarea_id = identifier
+
         return walkerarea
+
+    async def _handle_additional_keys(self, db_entry: SettingsWalkerarea, key: str, value) -> bool:
+        if key == "walker_id":
+            # Make sure a SettingsWalkerToWalkerarea row is present, else create one to make sure a connection exists
+            walkerarea_mappings_of_walker: List[SettingsWalkerToWalkerarea] = await SettingsWalkerToWalkerareaHelper\
+                .get(self._session, self._get_instance_id(), value)
+            walkerarea_mappings_of_walker.sort(key=lambda x: x.area_order)
+            for walkerarea_mapping in walkerarea_mappings_of_walker:
+                if walkerarea_mapping.walkerarea_id == db_entry.walkerarea_id:
+                    # already present, nothing to do
+                    return True
+
+            mapping: SettingsWalkerToWalkerarea = SettingsWalkerToWalkerarea()
+            mapping.walkerarea_id = db_entry.walkerarea_id
+            mapping.walker_id = value
+            order = walkerarea_mappings_of_walker[-1].area_order + 1
+            mapping.area_order = order
+            self._save(mapping)
+            return True
+        return False
