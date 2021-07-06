142903ce51754fdcdb14df324f85317ff2fda4fc
Grennith <till@mueskro.de>
2021-07-06 18:11:14 +0200

    master 3d606663ad711ab96c9a6999e362e199c7f6a2b2

---
+++ alembic/versions/415b64e87252_remove_inventory_clear_item_amount_tap_.py
@@ -0,0 +1,25 @@
+"""Remove inventory_clear_item_amount_tap_duration
+
+Revision ID: 415b64e87252
+Revises: 
+Create Date: 2021-07-06 18:04:30.271269
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '415b64e87252'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_column('settings_devicepool', 'inventory_clear_item_amount_tap_duration')
+
+
+def downgrade():
+    op.add_column('settings_devicepool', sa.Column('inventory_clear_item_amount_tap_duration', sa.Integer,
+                                                   default=None))
