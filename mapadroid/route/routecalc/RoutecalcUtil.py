b9fc23e1c17aeb6e59e16cc8d7a39f757ec2d17f
Grennith <till@mueskro.de>
2021-08-24 13:45:41 +0200

    WIP: Run routecalc in default executor

--- mapadroid/route/routecalc/RoutecalcUtil.py
+++ mapadroid/route/routecalc/RoutecalcUtil.py
@@ -85,9 +85,8 @@ class RoutecalcUtil:
             from mapadroid.route.routecalc.calculate_route_all import \
                 route_calc_all
             loop = asyncio.get_running_loop()
-            with concurrent.futures.ProcessPoolExecutor() as pool:
-                sol_best = await loop.run_in_executor(
-                    pool, route_calc_all, less_coords, route_name, num_processes, algorithm)
+            sol_best = await loop.run_in_executor(
+                None, route_calc_all, less_coords, route_name, num_processes, algorithm)
 
             end = timer()
 
