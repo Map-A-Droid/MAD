7c8c7dd8a643b80516dda005b72c06b6d99f0bb9
Grennith <till@mueskro.de>
2021-09-05 09:01:55 +0200

    Remove potentially unused /status endpoint of mitmreceiver

--- mapadroid/mitm_receiver/endpoints/__init__.py
+++ mapadroid/mitm_receiver/endpoints/__init__.py
@@ -3,11 +3,9 @@ from aiohttp import web
 from mapadroid.mitm_receiver.endpoints.GetAddressesEndpoint import GetAddressesEndpoint
 from mapadroid.mitm_receiver.endpoints.GetLatestEndpoint import GetLatestEndpoint
 from mapadroid.mitm_receiver.endpoints.ReceiveProtosEndpoint import ReceiveProtosEndpoint
-from mapadroid.mitm_receiver.endpoints.StatusEndpoint import StatusEndpoint
 
 
 def register_mitm_receiver_root_endpoints(app: web.Application):
     app.router.add_view('/get_addresses', GetAddressesEndpoint, name='get_addresses')
     app.router.add_view('/', ReceiveProtosEndpoint, name='receive_protos')
     app.router.add_view('/get_latest_mitm', GetLatestEndpoint, name='get_latest_mitm')
-    app.router.add_view('/status', StatusEndpoint, name='status')
