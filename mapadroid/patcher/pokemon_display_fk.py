7572ffa69b472b71e56f9f4a0378c5300794e863
fosJoddie <5710881+fosJoddie@users.noreply.github.com>
2021-09-12 12:45:37 +0200

    Clean up pokemon_display

--- mapadroid/patcher/pokemon_display_fk.py
+++ mapadroid/patcher/pokemon_display_fk.py
@@ -7,14 +7,19 @@ class Patch(PatchBase):
     def _execute(self):
         # Adding column quest_title for trs_quest
         if not self._schema_updater.check_column_exists('trs_quest', 'quest_title'):
-            query = (
-                "ALTER TABLE pokemon_display "
-                "ADD CONSTRAINT pokemon_encounter_id_fk FOREIGN KEY (`encounter_id`) "
-                "REFERENCES `pokemon` (`encounter_id`) "
-                "ON UPDATE CASCADE ON DELETE CASCADE"
-            )
             try:
-                self._db.execute(query, commit=True)
+                # make sure no lines are left in pokemon_display that are no longer in pokemon table
+                self._db.execute((
+                    "DELETE pd FROM pokemon_display pd"
+                    " LEFT JOIN pokemon ON pd.encounter_id=pokemon.encounter_id"
+                    " WHERE pokemon.pokemon_id IS NULL"
+                ), commit=True)
+                self._db.execute((
+                    "ALTER TABLE pokemon_display "
+                    "ADD CONSTRAINT pokemon_encounter_id_fk FOREIGN KEY (`encounter_id`) "
+                    "REFERENCES `pokemon` (`encounter_id`) "
+                    "ON UPDATE CASCADE ON DELETE CASCADE"
+                ), commit=True)
             except Exception as e:
                 self._logger.exception("Unexpected error: {}", e)
                 self.issues = True
