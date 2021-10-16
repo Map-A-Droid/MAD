ac24b2c29a80df602f47bcaa93d2acfaf2b128a3
Grennith <till@mueskro.de>
2021-10-16 16:45:04 +0200

    WIP: Quest title (#1189)  Master 9d9bd338d8679af7e2b4104791a3b728a6cc1c70

---
+++ alembic/versions/867783e63a90_add_quest_title_column.py
@@ -0,0 +1,32 @@
+"""Add quest title column
+
+Revision ID: 867783e63a90
+Revises: 5fc3214266b4
+Create Date: 2021-10-16 16:23:50.841025
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '867783e63a90'
+down_revision = '5fc3214266b4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    try:
+        op.add_column('trs_quest', sa.Column('quest_title', sa.String(100, 'utf8mb4_unicode_ci'), default=None,
+                                             nullable=True))
+    except Exception as e:
+        print("Failed adding column quest_title to trs_quest. Likely was placed there beforehand (master branch)")
+
+
+def downgrade():
+    try:
+        op.drop_column('trs_quest', 'quest_title')
+    except Exception as e:
+        print("Failed dropping column quest_title of trs_quest.")
+
