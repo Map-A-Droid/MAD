a57dcdd8364b18422af7a52514e046741e445d67
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-03-18 10:26:57 +0100

    Update QuestStrategy.py

--- mapadroid/worker/strategy/quest/QuestStrategy.py
+++ mapadroid/worker/strategy/quest/QuestStrategy.py
@@ -518,6 +518,11 @@ class QuestStrategy(AbstractMitmBaseStrategy, ABC):
         scanmode = "quests"
         injected_settings["scanmode"] = scanmode
         ids_iv: List[int] = []
+        routemanager_settings = await self._mapping_manager.routemanager_get_settings(self._area_id)
+        if routemanager_settings is not None:
+            # TODO: Moving to async
+            ids_iv = self._mapping_manager.get_monlist(self._area_id)
+
         self._encounter_ids = {}
         await self._mitm_mapper.update_latest(worker=self._worker_state.origin, key="ids_encountered",
                                               value=self._encounter_ids)
