5377a7fa3d2e82affd5ebd52cb9e56eac6c72600
Till Skrodzki <till@mueskro.de>
2022-05-24 11:10:48 +0200

    Fix MitmMapperServer.py accessing mitm_data_handler

--- mapadroid/data_handler/mitm_data/MitmMapper.py
+++ mapadroid/data_handler/mitm_data/MitmMapper.py
@@ -9,47 +9,47 @@ from mapadroid.utils.collections import Location
 
 class MitmMapper(AbstractMitmMapper):
     def __init__(self):
-        self.__mitm_data_handler: MitmDataHandler = MitmDataHandler()
+        self._mitm_data_handler: MitmDataHandler = MitmDataHandler()
 
     # ##
     # Data related methods
     # ##
     async def get_last_possibly_moved(self, worker: str) -> int:
-        return await self.__mitm_data_handler.get_last_possibly_moved(worker)
+        return await self._mitm_data_handler.get_last_possibly_moved(worker)
 
     async def update_latest(self, worker: str, key: str, value: Union[list, dict], timestamp_received_raw: float = None,
                             timestamp_received_receiver: float = None, location: Location = None) -> None:
         loop = asyncio.get_running_loop()
-        loop.run_in_executor(None, self.__mitm_data_handler.update_latest, worker, key, value, timestamp_received_raw,
+        loop.run_in_executor(None, self._mitm_data_handler.update_latest, worker, key, value, timestamp_received_raw,
                              timestamp_received_receiver, location)
 
     async def request_latest(self, worker: str, key: str,
                              timestamp_earliest: Optional[int] = None) -> Optional[LatestMitmDataEntry]:
-        return self.__mitm_data_handler.request_latest(worker, key, timestamp_earliest)
+        return self._mitm_data_handler.request_latest(worker, key, timestamp_earliest)
 
     async def get_poke_stop_visits(self, worker: str) -> int:
-        return await self.__mitm_data_handler.get_poke_stop_visits(worker)
+        return await self._mitm_data_handler.get_poke_stop_visits(worker)
 
     async def get_level(self, worker: str) -> int:
-        return await self.__mitm_data_handler.get_level(worker)
+        return await self._mitm_data_handler.get_level(worker)
 
     async def get_injection_status(self, worker: str) -> bool:
-        return await self.__mitm_data_handler.get_injection_status(worker)
+        return await self._mitm_data_handler.get_injection_status(worker)
 
     async def set_injection_status(self, worker: str, status: bool) -> None:
-        await self.__mitm_data_handler.set_injection_status(worker, status)
+        await self._mitm_data_handler.set_injection_status(worker, status)
 
     async def get_last_known_location(self, worker: str) -> Optional[Location]:
-        return self.__mitm_data_handler.get_last_known_location(worker)
+        return self._mitm_data_handler.get_last_known_location(worker)
 
     async def set_level(self, worker: str, level: int) -> None:
-        await self.__mitm_data_handler.set_level(worker, level)
+        await self._mitm_data_handler.set_level(worker, level)
 
     async def set_pokestop_visits(self, worker: str, pokestop_visits: int) -> None:
-        await self.__mitm_data_handler.set_pokestop_visits(worker, pokestop_visits)
+        await self._mitm_data_handler.set_pokestop_visits(worker, pokestop_visits)
 
     async def set_quests_held(self, worker: str, quests_held: Optional[List[int]]) -> None:
-        await self.__mitm_data_handler.set_quests_held(worker, quests_held)
+        await self._mitm_data_handler.set_quests_held(worker, quests_held)
 
     async def get_quests_held(self, worker: str) -> Optional[List[int]]:
-        return await self.__mitm_data_handler.get_quests_held(worker)
+        return await self._mitm_data_handler.get_quests_held(worker)
