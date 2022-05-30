829202d02ae8e88900e4f5f5f245fef0633b963d
Till Skrodzki <till@mueskro.de>
2022-05-30 16:05:26 +0200

    Remove autocommand being set by the type of the job to be executed

--- mapadroid/madmin/endpoints/routes/control/JobForWorkerEndpoint.py
+++ mapadroid/madmin/endpoints/routes/control/JobForWorkerEndpoint.py
@@ -16,14 +16,13 @@ class JobForWorkerEndpoint(AbstractControlEndpoint):
     async def get(self):
 
         jobname: Optional[str] = self.request.query.get('jobname')
-        job_type: Optional[str] = self.request.query.get('type')
         devices: Optional[List[str]] = self.request.query.getall('device[]')
         if not devices:
             await self._add_notice_message('No devices specified')
             await self._redirect(self._url_for('install_status'))
 
         for device in devices:
-            await self._get_device_updater().add_job(device, jobname, job_type)
+            await self._get_device_updater().add_job(device, jobname)
 
         await self._add_notice_message('Job successfully queued')
         await self._redirect(self._url_for('install_status'))
