8e7c807639719ca452e0808232adb7ffc2d01eba
JabLuszko <JabLuszko@users.noreply.github.com>
2022-06-27 20:13:26 +0200

    Link to async version of PD/RGC.
    I guess this is gonna be standard for now, right? Just make life easier I guess :)

--- mapadroid/utils/global_variables.py
+++ mapadroid/utils/global_variables.py
@@ -3,8 +3,8 @@
 # MAD APK related elements
 CHUNK_MAX_SIZE = 1024 * 1024 * 8  # 8MiB
 MAD_APK_ALLOWED_EXTENSIONS = {'apk', 'zip'}
-URL_RGC_APK = 'https://raw.githubusercontent.com/Map-A-Droid/MAD/master/APK/RemoteGpsController.apk'
-URL_PD_APK = 'https://www.maddev.eu/apk/PogoDroid.apk'
+URL_RGC_APK = 'https://raw.githubusercontent.com/Map-A-Droid/MAD/async_quest_layers/APK/RemoteGpsController.apk'
+URL_PD_APK = 'https://www.maddev.eu/apk/PogoDroid_async.apk'
 BACKEND_SUPPORTED_VERSIONS = "https://auth.maddev.eu/thirdparty/supported_versions"
 
 VERSIONCODES_URL = 'https://raw.githubusercontent.com/Map-A-Droid/MAD/master/configs/version_codes.json'
