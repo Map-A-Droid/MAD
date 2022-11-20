b3fe8e56a34bbb37db89b8416929d3be1234c2c5
Till Skrodzki <till@mueskro.de>
2022-11-20 19:36:45 +0100

    Remove garbage collection...

--- mapadroid/utils/SystemStatsUtil.py
+++ mapadroid/utils/SystemStatsUtil.py
@@ -1,39 +1,20 @@
 import asyncio
 import calendar
 import datetime
-import gc
 import os
-from threading import active_count
 
 import psutil
 
 from mapadroid.db.helper.TrsUsageHelper import TrsUsageHelper
-from mapadroid.utils.logging import get_logger, LoggerEnums
+from mapadroid.utils.logging import LoggerEnums, get_logger
 from mapadroid.utils.madGlobals import application_args, terminate_mad
 
 logger = get_logger(LoggerEnums.system)
 
 
-def find_referring_graphs(obj):
-    ignore_elems = [locals(), globals(), gc.garbage]
-
-    referrers = (r for r in gc.get_referrers(obj) if r not in ignore_elems)
-    for ref in referrers:
-        print(type(ref))
-        if isinstance(ref, Graph):  # noqa: F821
-            # A graph node
-            yield ref
-        elif isinstance(ref, dict):
-            # An instance or other namespace dictionary
-            for parent in find_referring_graphs(ref):
-                yield parent
-
-
 async def get_system_infos(db_wrapper):
     pid = os.getpid()
     process_running = psutil.Process(pid)
-    gc.set_threshold(5, 1, 1)
-    gc.enable()
     await asyncio.sleep(60)
     if application_args.trace:
         import tracemalloc
@@ -41,10 +22,10 @@ async def get_system_infos(db_wrapper):
     while not terminate_mad.is_set():
         logger.debug('Starting internal Cleanup')
         loop = asyncio.get_running_loop()
-        collected, cpu_usage, mem_usage, unixnow = await loop.run_in_executor(
+        cpu_usage, mem_usage, unixnow = await loop.run_in_executor(
             None, __run_system_stats, process_running)
         async with db_wrapper as session, session:
-            await TrsUsageHelper.add(session, application_args.status_name, cpu_usage, mem_usage, collected, unixnow)
+            await TrsUsageHelper.add(session, application_args.status_name, cpu_usage, mem_usage, 0, unixnow)
             await session.commit()
         await asyncio.sleep(application_args.statistic_interval)
 
@@ -54,8 +35,8 @@ initial_snapshot = None
 
 
 def display_top(snapshot, key_type='traceback', limit=30):
-    import tracemalloc
     import linecache
+    import tracemalloc
     snapshot = snapshot.filter_traces((
         tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
         tracemalloc.Filter(False, "<unknown>"),
@@ -82,27 +63,11 @@ def display_top(snapshot, key_type='traceback', limit=30):
 
 def __run_system_stats(py):
     global last_snapshot, initial_snapshot
-    logger.debug('Collecting...')
-    unreachable_objs = gc.collect()
-    logger.debug('Unreachable objects: {} - Remaining garbage: {} - Running threads: {}',
-                 str(unreachable_objs), str(gc.garbage), str(active_count()))
-    for obj in gc.garbage:
-        for ref in find_referring_graphs(obj):
-            ref.set_next(None)
-            del ref  # remove local reference so the node can be deleted
-        del obj  # remove local reference so the node can be deleted
-
-    # Clear references held by gc.garbage
-    logger.debug('Clearing gc garbage')
-    del gc.garbage[:]
     mem_usage = py.memory_info()[0] / 2. ** 30
     cpu_usage = py.cpu_percent()
     logger.info('Instance name: "{}" - Memory usage: {:.3f} GB - CPU usage: {}',
                 str(application_args.status_name), mem_usage, str(cpu_usage))
-    collected = None
-    if application_args.stat_gc:
-        collected = gc.collect()
-        logger.debug("Garbage collector: collected %d objects." % collected)
+
     zero = datetime.datetime.utcnow()
     unixnow = calendar.timegm(zero.utctimetuple())
 
@@ -136,37 +101,37 @@ def __run_system_stats(py):
                 logger.info(stat)
         last_snapshot = new_snapshot
 
-    try:
-        import objgraph
-        logger.info("show_most_common_types")
-        objgraph.show_most_common_types(limit=50, shortnames=False)
-        logger.info("show_growth")
-        objgraph.show_growth(limit=50, shortnames=False)
-        logger.info("get_new_ids")
-        objgraph.get_new_ids(limit=50)
-        logger.info("Constructing backrefs graph")
-        # by_type = objgraph.by_type('builtins.list')
-        by_type = objgraph.by_type('StackSummary')
-        # by_type = objgraph.by_type('uvloop.Loop')
-        # by_type = objgraph.by_type("mapadroid.utils.collections.Location")
-        # by_type = objgraph.by_type("TrsSpawn")
-        if len(by_type) > 1:
-            by_type_empty = [type_filtered for type_filtered in by_type if not type_filtered]
-            # by_type_filled = [type_filtered for type_filtered in by_type if type_filtered and "mapadroid" in type_filtered.filename]
-            by_type_filled = [type_filtered for type_filtered in by_type if type_filtered]
-            logger.warning("Filled: {}, empty: {}, total: {}", len(by_type_filled), len(by_type_empty),
-                           len(by_type))
-            obj = by_type[-500:]
-            # TODO: Filter for lists of dicts...
-            # filtered = [type_filtered for type_filtered in by_type if len(type_filtered) > 50]
-            del by_type_empty
-            del by_type_filled
-            del by_type
-            # objgraph.show_backrefs(obj, max_depth=10)
-            # objgraph.show_backrefs(obj, max_depth=5)
-        else:
-            logger.warning("Not enough of type to show: {}", len(by_type))
-    except Exception as e:
-        pass
+        try:
+            import objgraph
+            logger.info("show_most_common_types")
+            objgraph.show_most_common_types(limit=50, shortnames=False)
+            logger.info("show_growth")
+            objgraph.show_growth(limit=50, shortnames=False)
+            logger.info("get_new_ids")
+            objgraph.get_new_ids(limit=50)
+            logger.info("Constructing backrefs graph")
+            # by_type = objgraph.by_type('builtins.list')
+            by_type = objgraph.by_type('StackSummary')
+            # by_type = objgraph.by_type('uvloop.Loop')
+            # by_type = objgraph.by_type("mapadroid.utils.collections.Location")
+            # by_type = objgraph.by_type("TrsSpawn")
+            if len(by_type) > 1:
+                by_type_empty = [type_filtered for type_filtered in by_type if not type_filtered]
+                # by_type_filled = [type_filtered for type_filtered in by_type if type_filtered and "mapadroid" in type_filtered.filename]
+                by_type_filled = [type_filtered for type_filtered in by_type if type_filtered]
+                logger.warning("Filled: {}, empty: {}, total: {}", len(by_type_filled), len(by_type_empty),
+                               len(by_type))
+                obj = by_type[-500:]
+                # TODO: Filter for lists of dicts...
+                # filtered = [type_filtered for type_filtered in by_type if len(type_filtered) > 50]
+                del by_type_empty
+                del by_type_filled
+                del by_type
+                # objgraph.show_backrefs(obj, max_depth=10)
+                # objgraph.show_backrefs(obj, max_depth=5)
+            else:
+                logger.warning("Not enough of type to show: {}", len(by_type))
+        except Exception as e:
+            pass
     logger.info("Done with GC")
-    return collected, cpu_usage, mem_usage, unixnow
+    return cpu_usage, mem_usage, unixnow
