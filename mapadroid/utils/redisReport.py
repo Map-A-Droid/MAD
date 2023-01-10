c0c17d7fee11f29290d5618d9e0ac5ac6c81dfac
JabLuszko <you@example.com>
2023-01-10 22:02:20 +0100

    Cleanup comments

--- mapadroid/utils/redisReport.py
+++ mapadroid/utils/redisReport.py
@@ -8,13 +8,9 @@ logger = get_logger(LoggerEnums.system)
 async def report_queue_size(__db_wrapper, __queueObject):
     __cache_key = application_args.redis_report_queue_key
     __sleep_time = application_args.redis_report_queue_interval
-    logger.info("Reporting queue size to redis via key {} starting", __cache_key)
     while not terminate_mad.is_set():
-        logger.info('Reporting queue size to redis while not starting')
         __cache: Redis = await __db_wrapper.get_cache()
         __value = __queueObject.qsize()
-        logger.info('Reporting queue size to redis while not value: {}', __value)
         await __cache.set(__cache_key, __value, ex=__sleep_time*2)
-        logger.info('Reporting queue size to redis sleeping for: {}', __sleep_time)
         await asyncio.sleep(__sleep_time)
 
