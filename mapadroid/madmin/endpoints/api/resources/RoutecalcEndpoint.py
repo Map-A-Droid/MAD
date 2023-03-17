3f213ba72c55f01ab661e6738f2e083886bbfe21
JabLuszko <JabLuszko@users.noreply.github.com>
2023-03-17 21:19:51 +0100

    Update RoutecalcEndpoint.py

--- mapadroid/madmin/endpoints/api/resources/RoutecalcEndpoint.py
+++ mapadroid/madmin/endpoints/api/resources/RoutecalcEndpoint.py
@@ -13,7 +13,7 @@ class RoutecalcEndpoint(AbstractResourceEndpoint):
         db_wrapper: DbWrapper = self._get_db_wrapper()
         areas: Dict[int, SettingsArea] = await db_wrapper.get_all_areas(self._session)
         areas_with_routecalc: List[SettingsArea] = []
-        for area_id, area in areas.values():
+        for area_id, area in areas.items():
             routecalc_id: Optional[int] = getattr(area, "routecalc")
             if routecalc_id and routecalc_id == db_entry.routecalc_id:
                 areas_with_routecalc.append(area)
