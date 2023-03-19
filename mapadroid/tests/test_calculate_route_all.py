bf38192f4169fe69d88d4795c09b092007ddb2e1
Till Skrodzki <till@mueskro.de>
2023-03-19 08:13:17 +0100

    Use IsolatedAsyncioTestCase for route calc

--- mapadroid/tests/test_calculate_route_all.py
+++ mapadroid/tests/test_calculate_route_all.py
@@ -1,19 +1,19 @@
+import unittest
 from typing import List
-from unittest import TestCase
 
 from mapadroid.route.routecalc.calculate_route_all import route_calc_all
 from mapadroid.utils.collections import Location
 from mapadroid.utils.madGlobals import RoutecalculationTypes
 
 
-class TestRouteCalcAll(TestCase):
-    def test_route_calc_all(self):
+class TestRouteCalcAll(unittest.IsolatedAsyncioTestCase):
+    async def test_route_calc_all(self):
         coords: List[Location] = []
         coords.append(Location(52.519903, 13.400699))
         coords.append(Location(52.526848, 13.392288))
         coords.append(Location(52.525230, 13.409111))
         coords.append(Location(52.518910, 13.420354))
         coords.append(Location(52.514836, 13.409282))
-        route_calc_all(coords, "foo", RoutecalculationTypes.OR_TOOLS)
+        await route_calc_all(coords, "foo", RoutecalculationTypes.OR_TOOLS)
 
         # self.fail()
