8a7cd3abf11e96108da7d1e985b70ccf74397033
Till Skrodzki <till@mueskro.de>
2023-03-27 05:30:46 +0200

    Convert algo_value to int

--- mapadroid/utils/routeutil.py
+++ mapadroid/utils/routeutil.py
@@ -68,14 +68,19 @@ def pre_check_value(walker_settings: SettingsWalkerarea, eventid, location: Opti
     if workers_registered_to_route == 0 and coords_scannable == 0:
         logger.warning("Area is not being scanned right now and no coords are left to be scanned")
         return False
-    if walker_settings.algo_type in ('timer', 'period', 'coords', 'idle'):
-        walkervalue = walker_settings.algo_value
-        if walkervalue is None and walker_settings.algo_type == 'coords' or len(walkervalue) == 0:
-            return True
-        return check_walker_value_type(walkervalue, location)
-    elif walker_settings.algo_type == "round" and (walker_settings.algo_value is None
-                                                   or walker_settings.algo_value >= rounds_processed):
-        logger.warning("Area {} was scanned for {} rounds with max rounds set to {}",
-                       walker_settings.area_id, rounds_processed, walker_settings.algo_value)
+    try:
+        if walker_settings.algo_type in ('timer', 'period', 'coords', 'idle'):
+            walkervalue = walker_settings.algo_value
+            if walkervalue is None and walker_settings.algo_type == 'coords' or len(walkervalue) == 0:
+                return True
+            return check_walker_value_type(walkervalue, location)
+        elif walker_settings.algo_type == "round" and (walker_settings.algo_value is None
+                                                       or int(walker_settings.algo_value) >= rounds_processed):
+            logger.warning("Area {} was scanned for {} rounds with max rounds set to {}",
+                           walker_settings.area_id, rounds_processed, walker_settings.algo_value)
+            return False
+    except ValueError as e:
+        logger.warning("Failed checking algo_type '{}' and algo_value '{}': {}",
+                       walker_settings.algo_type, walker_settings.algo_value, e)
         return False
     return True
