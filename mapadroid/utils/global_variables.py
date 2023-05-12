bd22e2f8ba0d31625a28c7010beaa346abee5edd
Till Skrodzki <till@mueskro.de>
2023-05-12 05:53:58 +0200

    Update global_variables.py

--- mapadroid/utils/global_variables.py
+++ mapadroid/utils/global_variables.py
@@ -3,8 +3,8 @@
 # MAD APK related elements
 CHUNK_MAX_SIZE = 1024 * 1024 * 8  # 8MiB
 MAD_APK_ALLOWED_EXTENSIONS = set(['apk', 'zip'])
-URL_RGC_APK = 'https://raw.githubusercontent.com/Map-A-Droid/MAD/master/APK/RemoteGpsController.apk'
-URL_PD_APK = 'https://www.maddev.eu/apk/PogoDroid.apk'
+URL_RGC_APK = 'https://raw.githubusercontent.com/Map-A-Droid/MAD/legacy/APK/RemoteGpsController.apk'
+URL_PD_APK = 'https://www.maddev.eu/apk/PogoDroid_legacy.apk'
 BACKEND_SUPPORTED_VERSIONS = "https://auth.maddev.eu/thirdparty/supported_versions"
 
 VERSIONCODES_URL = 'https://raw.githubusercontent.com/Map-A-Droid/MAD/master/configs/version_codes.json'
