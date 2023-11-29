db774887a21044583ee5a69a523987632a0fd829
Till Skrodzki <till@mueskro.de>
2023-11-29 20:46:38 +0100

    Fix typo for wrong variable

--- mapadroid/websocket/communicator.py
+++ mapadroid/websocket/communicator.py
@@ -280,7 +280,7 @@ class Communicator(AbstractCommunicator):
                 # https://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
                 ipv6_search = re.search('(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))', res)
                 if ipv6_search:
-                    ip_address_found = found.group(1)
+                    ip_address_found = ipv6_search.group(1)
         except Exception as e:
             logger.error(f"Failed parsing external IP: {e}")
             return None
